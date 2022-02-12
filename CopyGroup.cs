3708d6340ede1300f25fc1687634d77162382920
Алексей Захаров <zakharov.nnov@gmail.com>
2022-02-12 16:11:13 +0300

    обновлено согласно заданию 3

--- CopyGroup.cs
+++ CopyGroup.cs
@@ -1,6 +1,8 @@
 ﻿using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Architecture;
 using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,23 +16,97 @@ namespace CopyGroupPlugin
     {
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
-            UIDocument uiDoc = commandData.Application.ActiveUIDocument;
-            Document doc = uiDoc.Document;
-
-            Reference reference = uiDoc.Selection.PickObject(Autodesk.Revit.UI.Selection.ObjectType.Element, "Выберите группу объектов");
-            Element element = doc.GetElement(reference);
-            Group group = element as Group;
-
-            XYZ point = uiDoc.Selection.PickPoint("Выберите точку");
-
-            using (Transaction ts = new Transaction(doc, "Copy group"))
+            try
             {
-                ts.Start("Копирование группы объектов");
-                doc.Create.PlaceGroup(point, group.GroupType);
-                ts.Commit();
+                UIDocument uiDoc = commandData.Application.ActiveUIDocument;
+                Document doc = uiDoc.Document;
 
+                GroupPickFilter groupPickFilter = new GroupPickFilter();
+                Reference reference = uiDoc.Selection.PickObject(ObjectType.Element, groupPickFilter,"Выберите группу объектов");
+                Element element = doc.GetElement(reference);
+
+                Group group = element as Group;
+                XYZ groupCenter = GetElementCenter(group);
+                Room room = GetRoomByPoint(doc, groupCenter);
+                XYZ roomCenter = GetElementCenter(room);
+                XYZ offset = groupCenter - roomCenter;
+
+                XYZ point = uiDoc.Selection.PickPoint("Выберите точку");
+                XYZ insertPoint = null;
+
+                Room newRoom = GetRoomByPoint(doc, point);
+                if (newRoom!=null)
+                {
+                    XYZ newRoomCenter = GetElementCenter(newRoom);
+                    insertPoint = newRoomCenter + offset;
+                }
+                else
+                {
+                    message += "Точка вставки должна находится в пределах комнаты";
+                    return Result.Failed;
+                }
+
+                using (Transaction ts = new Transaction(doc, "Copy group"))
+                {
+                    ts.Start("Копирование группы объектов");
+                    doc.Create.PlaceGroup(insertPoint, group.GroupType);
+                    ts.Commit();
+                }
             }
+            catch (Autodesk.Revit.Exceptions.OperationCanceledException)
+            {
+                return Result.Cancelled;
+            }
+            catch (Exception ex)
+            {
+                message = ex.Message;
+                return Result.Failed;
+            }
+
             return Result.Succeeded;
+        }
+
+        public XYZ GetElementCenter(Element element)
+        {
+            BoundingBoxXYZ bounding = element.get_BoundingBox(null);
+            return (bounding.Max + bounding.Min) / 2;
+        }
+
+        public Room GetRoomByPoint(Document doc, XYZ point)
+        {
+            FilteredElementCollector collector = new FilteredElementCollector(doc);
+            collector.OfCategory(BuiltInCategory.OST_Rooms);
+            foreach (Element elem in collector)
+            {
+                Room room = elem as Room;
+                if (room!=null)
+                {
+                    if (room.IsPointInRoom(point))
+                    {
+                        return room;
+                    }
+                }
+            }
+            return null;
+        }
+    }
+    public class GroupPickFilter : ISelectionFilter
+    {
+        public bool AllowElement(Element elem)
+        {
+            if (elem.Category.Id.IntegerValue == (int)BuiltInCategory.OST_IOSModelGroups)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool AllowReference(Reference reference, XYZ position)
+        {
+            return false; ;
         }
     }
 }
